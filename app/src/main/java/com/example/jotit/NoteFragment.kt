1c6e2dca204b583de0dc3593a8e249649c49c615
DTechist <achomikechukwu@gmail.com>
2023-09-12 16:22:45 +0100

    Initial commit to set up project

---
+++ app/src/main/java/com/example/jotit/NoteFragment.kt
@@ -0,0 +1,11 @@
+package com.example.jotit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class NoteFragment : Fragment() {
+
+}
